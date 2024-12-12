f64112d94123e48a1529464ff052659a190fc097
blockninja124 <64211894+blockninja124@users.noreply.github.com>
2024-12-12 22:07:16 +0000

    Update MixinShipMountingEntity.java
    Fix da code a bit

--- src/main/java/com/mj/vsegamepad/mixin/MixinShipMountingEntity.java
+++ src/main/java/com/mj/vsegamepad/mixin/MixinShipMountingEntity.java
@@ -1,45 +1,44 @@
 package com.mj.vsegamepad.mixin;
 
 import com.mj.vsegamepad.VSKeyBindings;
-import com.mojang.math.Vector3f;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.Options;
+import org.joml.Vector3f;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.valkyrienskies.mod.common.entity.ShipMountingEntity;
 import org.valkyrienskies.mod.common.networking.PacketPlayerDriving;
-import org.valkyrienskies.mod.common.vsCore;
 
 @Mixin(ShipMountingEntity.class)
 public abstract class MixinShipMountingEntity {
 
+
     @Inject(method = "sendDrivingPacket", at = @At("HEAD"), cancellable = true)
     private void sendDrivingPacket(CallbackInfo ci) {
-        if (!Minecraft.getInstance().level.isClientSide) {
-            ci.cancel();
-            return;
-        }
+        // Trick to get values from the class we mixining
+        ShipMountingEntity self = ((ShipMountingEntity) (Object) this);
+
+        if (!self.level.isClientSide()) return;
 
         Options opts = Minecraft.getInstance().options;
-        boolean forward = VSKeyBindings.shipUp.isDown();
-        boolean backward = VSKeyBindings.shipDown.isDown();
-        boolean left = VSKeyBindings.shipLeft.isDown();
-        boolean right = VSKeyBindings.shipRight.isDown();
+        boolean forward = opts.keyUp.isDown();
+        boolean backward = opts.keyDown.isDown();
+        boolean left = opts.keyLeft.isDown();
+        boolean right = opts.keyRight.isDown();
+        boolean up = opts.keyJump.isDown();
+        boolean sprint = self.getControllingPassenger() != null && self.getControllingPassenger().isSprinting();
+        boolean down = VSKeyBindings.shipDown.isDown();
+        // TODO: replace me with a gamepad keybinding for cruise
+        boolean cruise = org.valkyrienskies.mod.common.config.VSKeyBindings.INSTANCE.getShipCruise().get().isDown();
 
         Vector3f impulse = new Vector3f();
-        impulse.set(
-                left == right ? 0.0f : (left ? -1.0f : 1.0f),
-                0.0f,
-                forward == backward ? 0.0f : (forward ? 1.0f : -1.0f)
-        );
+        impulse.z = (forward == backward) ? 0.0f : (forward ? 1.0f : -1.0f);
+        impulse.x = (left == right) ? 0.0f : (left ? 1.0f : -1.0f);
+        impulse.y = (up == down) ? 0.0f : (up ? 1.0f : -1.0f);
 
-        with(vsCore.simplePacketNetworking) {
-            PacketPlayerDriving packet = new PacketPlayerDriving(impulse, false, false);
-            packet.sendToServer();
-        }
-
-        ci.cancel();
+        // TODO: Fix me, we are too deprecated to compile :sob:
+        new PacketPlayerDriving(impulse, sprint, cruise).sendToServer();
     }
 }
